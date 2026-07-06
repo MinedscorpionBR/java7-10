5d9269cbd57ba867f4386c2d762dcc897e649f1e
Minedscorpion <69402472+MinedscorpionBR@users.noreply.github.com>
2026-07-06 16:29:03 -0300

    Add files via upload

--- src/pacote1/Main.java
+++ src/pacote1/Main.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pacote1;
 
@@ -9,5 +9,12 @@ package pacote1;
  * @author proza
  */
 public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
